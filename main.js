f35de9cb2a950c923657a0c45ab475529f51d3d1
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-02-29 22:19:45 -0300

    Feature(message-flow): Message flows through the functions without problems

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import messageReceiver from './messageReceiver';
 
 const message = "How are you doing today?";
+const offset = 10;
 
-const cipheredMessage = messageReceiver(message);
+const cipheredMessage = messageReceiver(message, offset);
 
 // console.log(`Original message: ${message}`);
 // console.log(`Ciphered message: ${cipheredMessage}`);
